b093a664a8f975812c204af341bb83ead94ce61b
Tony Nguyen <tonngu137@gmail.com>
2025-04-17 08:14:21 +0200

    second commit

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,53 +1,45 @@
-const path = require("path");
-const glob = require("glob-all");
+const path = require('path');
+const glob = require('glob-all');
 
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
-const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { PurgeCSSPlugin } = require('purgecss-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const PATHS = {
-  js: path.join(__dirname, "src/"),
-  views: path.join(__dirname, "views"),
-  root: path.join(__dirname, ".."),
+  js: path.join(__dirname, 'src/'),
+  views: path.join(__dirname, 'views'),
+  root: path.join(__dirname, '..'),
 };
 
 console.log(PATHS.root);
 
 module.exports = {
-  mode: "production",
+  mode: 'production',
   entry: {
-    index: "./src/js/index.js",
+    index: './src/js/index.js',
   },
   stats: {
-    loggingDebug: ["sass-loader"],
+    loggingDebug: ['sass-loader'],
   },
   output: {
-    filename: "[name].min.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].min.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
 
   plugins: [
     new MiniCssExtractPlugin({
       filename: (pathData) => {
-        return pathData.chunk.name === "index"
-          ? "style.min.css"
-          : "[name].min.css";
+        return pathData.chunk.name === 'index' ? 'style.min.css' : '[name].min.css';
       },
     }),
-    // new PurgeCSSPlugin({
-    //   paths: glob.sync(
-    //     [
-    //       `${PATHS.root}/Private/Frontend/Partials/Page/**/*.html`,
-    //       `${PATHS.root}/Private/Mask/Frontend/Templates/**/*.html`,
-    //     ],
-    //     { nodir: true }
-    //   ),
-    //   safelist: {
-    //     standard: [/d-none/, /:marker/],
-    //     deep: [/strong/, /aria/],
-    //     greedy: [/strong/, /aria/, /contact-formular/, /r-c/],
-    //   },
-    // }),
+    new PurgeCSSPlugin({
+      paths: glob.sync([`${__dirname}/*.html`], { nodir: true }),
+      // safelist: {
+      //   standard: [/d-none/, /:marker/],
+      //   deep: [/strong/, /aria/],
+      //   greedy: [/strong/, /aria/, /contact-formular/, /r-c/],
+      // },
+    }),
   ],
   optimization: {
     minimizer: [
@@ -66,30 +58,30 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.scss$/i,
         use: [
           MiniCssExtractPlugin.loader, // 3. extract css into files
-          "css-loader", // 2. Turns css into commonjs
+          'css-loader', // 2. Turns css into commonjs
 
           {
-            loader: "postcss-loader",
+            loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: [["postcss-sort-media-queries"]],
+                plugins: [['postcss-sort-media-queries']],
               },
             },
           },
 
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
-              implementation: require("sass"),
+              implementation: require('sass'),
               sassOptions: {
                 // This silences the deprecation warning
-                silenceDeprecations: ["legacy-js-api"],
+                silenceDeprecations: ['legacy-js-api'],
               },
             },
           }, // 1. Turns sass into css
